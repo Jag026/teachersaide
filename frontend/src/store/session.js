8e39a08be2cfee4b58b38ebe7cca5795ba445830
Drew Griffin <drew.griffin.dev@gmail.com>
2023-01-15 22:52:38 -0600

    Add frontend signup form

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -54,5 +54,22 @@ export const restoreUser = () => async dispatch => {
     dispatch(setUser(data.user));
     return response;
   };
-  
+
+  export const signup = (user) => async (dispatch) => {
+    const { first_name, last_name, username, email, password } = user;
+    const response = await csrfFetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({
+        first_name,
+        last_name,
+        username,
+        email,
+        password,
+      }),
+    });
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+  };
+
 export default sessionReducer;
