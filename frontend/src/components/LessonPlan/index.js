2aba64fa3d72e6f3055f50204d03daa52ad166c5
Drew Griffin <drew.griffin.dev@gmail.com>
2023-01-18 09:34:29 -0600

    Update plan UI

--- frontend/src/components/LessonPlan/index.js
+++ frontend/src/components/LessonPlan/index.js
@@ -11,7 +11,13 @@ function LessonPlan(props) {
   const [subject, setSubject] = useState("");
   const [errors, setErrors] = useState([]);
   const [textContent, setTextContent] = useState("")
+  const [resetButton, setResetButton] = useState("")
+  const [visible, setVisible] = useState(false);
+  const [formVisible, setFormVisible] = useState(true);
 
+  const resetForm = () => {
+    window.location.reload();
+  };
 
   const handleSubmit = async(e) => {
     e.preventDefault();
@@ -19,46 +25,50 @@ function LessonPlan(props) {
     await setTextContent("Loading lesson plan...");
     let plan =  await dispatch(await sessionActions.fetchLessonplan({ grade, subject }))
     await setTextContent(<RichTextEditor text={plan.replace(/\n/g, '\n')} />)
+    await setVisible(true)
+    await setFormVisible(false)
+
     // return setErrors(['Confirm Password field must be the same as the Password field']);
   };
 
   return (
     <div className="flex flex-col items-center">
         <h2 className="font-serif text-5xl my-10">Teacher's AIde</h2>
-        <p className="p-5">An AI assistant that can quickly generate lesson plans for all grade levels. Enter a grade level and subject to get started</p>
+        <p className="p-5">A powerful AI assistant that can generate lessons plans for any class.</p>
         <p>Example: grade level: 11th || subject: science, molar mass</p>
-        <p>Example: grade level: 10th || subject: history, the French Revolution</p> 
-        <p>Example: grade level: 2nd || subject: math, addition an subtraction</p>
+        <p>Example: grade level: 7th || subject: history, the American Revolution</p> 
+        <p>Example: grade level: 3rd || subject: math, addition and subtraction</p>
 
-        <form onSubmit={handleSubmit} className="flex flex-col items-center">
+        {formVisible && <form onSubmit={handleSubmit} className="flex flex-col items-center mt-8">
           <ul>
             {errors.map((error, idx) => <li key={idx}>{error}</li>)}
           </ul>
           <div>
-          <label className="mx-4 text-xl">
+          <label className="mx-4 text-l">
             Enter a grade level
             <input
               type="text"
               value={grade}
               onChange={(e) => setGrade(e.target.value)}
               required
-              className="mx-2 border border-sky-500"
+              className="mx-2 h-10 w-14 p-2 border border-sky-500"
             />
           </label>
-          <label className="mx-4 text-xl">
+          <label className="mx-4 text-l">
             Enter a subject
             <input
               type="text"
               value={subject}
               onChange={(e) => setSubject(e.target.value)}
               required
-              className="mx-2 border border-sky-500"
+              className="mx-2 h-10 p-2 border border-sky-500"
             />
           </label>
           </div>
-          <button className="m-6 w-28 bg-slate-200 border text-l" type="submit">Get Plan</button>
-        </form>
-        <div>
+          <button className="m-6 w-28 h-10 bg-slate-300 border text-l" type="submit">Get Plan</button>
+        </form>}
+        <div className="w-3/5 mt-6">
+          {visible && <button className="m-6 w-28 h-10 bg-slate-300 border text-l" onClick={resetForm} type="submit">Reset</button>}
           {textContent}
         </div>
     </div>
