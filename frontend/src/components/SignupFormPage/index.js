3e34bdd4bc09356fa500ca531e78ae3f094f7aa1
Drew Griffin <drew.griffin.dev@gmail.com>
2023-01-23 10:01:55 -0600

    Update sign-up page ux:

--- frontend/src/components/SignupFormPage/index.js
+++ frontend/src/components/SignupFormPage/index.js
@@ -30,66 +30,71 @@ function SignupFormPage() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className="m-10 bg-white p-6 rounded-lg shadow-md flex flex-col sm:w-2/4 sm:max-w-[600px]">
       <ul>
         {errors.map((error, idx) => <li key={idx}>{error}</li>)}
       </ul>
-      <label>
+      <label className="p-2 mb-4">
         First Name
         <input
           type="text"
           value={first_name}
           onChange={(e) => setFirst_Name(e.target.value)}
           required
+          className="ml-2 border border-sky-500"
         />
       </label>
-      <label>
-      <label>
+      <label className="p-2 mb-4">
         Last Name
         <input
           type="text"
           value={last_name}
           onChange={(e) => setLast_Name(e.target.value)}
           required
+          className="ml-2 border border-sky-500"
         />
       </label>
-      <label></label>
+      <label className="p-2 mb-4">
         Email
         <input
           type="text"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           required
+          className="ml-2 border border-sky-500"
         />
       </label>
-      <label>
+      <label className="p-2 mb-4">
         Username
         <input
           type="text"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
           required
+          className="ml-2 border border-sky-500"
         />
       </label>
-      <label>
+      <label className="p-2 mb-4">
         Password
         <input
           type="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           required
+          className="ml-2 border border-sky-500"
         />
       </label>
-      <label>
+      <label className="p-2 mb-4">
         Confirm Password
         <input
           type="password"
           value={confirmPassword}
           onChange={(e) => setConfirmPassword(e.target.value)}
           required
+          className="ml-2 border border-sky-500"
         />
       </label>
-      <button type="submit">Sign Up</button>
+      <button type="submit" className="w-20 mt-3 p-1 border border-sky-500">Sign Up</button>
     </form>
   );
 }
