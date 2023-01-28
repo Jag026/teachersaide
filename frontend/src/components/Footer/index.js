09aa08290552e4f8c620bffff221b589177064e6
Drew Griffin <drew.griffin.dev@gmail.com>
2023-01-27 18:15:08 -0600

    Add footer

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Footer = () => {
   return (
     <footer className="bg-red-500 h-full mt-60">
-      <div className="bg-red-500 h-5/6 py-8">
+      <div className="bg-red-500 h-5/6 py-8 flex">
         <p className="p-5">Copyright © {new Date().getFullYear()} Teachersaide.io</p>
+        <NavLink exact to="/policies" className="p-5 ml-10">Site Policies</NavLink>
       </div>
     </footer>
   );
