2c4b0aaeae50ea10d7d2a5a0074abc9935f29d8f
Drew Griffin <drew.griffin.dev@gmail.com>
2023-02-09 21:37:17 -0600

    Add sitemap and robots

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const apiRouter = require('./api');
+const path = require("path");
 
 router.use('/api', apiRouter);
+
+//sitemap
+router.get("/sitemap.xml", (req, res) => {
+  const sitemapPath = path.resolve("routes/sitemap.xml");
+  res.sendFile(sitemapPath);
+});
+
+router.get('/robots.txt', function (req, res) {
+  res.type('text/plain');
+  const robotPath = path.resolve("routes/robots.txt");
+  res.sendFile(robotPath);
+  //res.send("User-agent: *\nDisallow: /specific-page\nAllow: /");
+});
 
 // Static routes
 // Serve React build files in production
