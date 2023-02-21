ac971cc9d7a8e0615b9011a9eed80cad1c92a141
Drew Griffin <drew.griffin.dev@gmail.com>
2023-02-20 22:13:11 -0600

    Add backend email

--- backend/config/index.js
+++ backend/config/index.js
@@ -5,6 +5,7 @@ module.exports = {
     api_key: process.env.API_KEY,
     post_secret: process.env.POST_SECRET,
     captcha_secret: process.env.CAPTCHA_SECRET,
+    send_email: process.env.SEND_EMAIL,
     jwtConfig: {
       secret: process.env.JWT_SECRET,
       expiresIn: process.env.JWT_EXPIRES_IN
--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -4,6 +4,7 @@ const { handleValidationErrors } = require('../../utils/validation');
 
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
+const { send_email } = require('../../config/index.js');
 
 
 const router = express.Router();
@@ -68,5 +69,25 @@ router.get(
   }
 );
 
+router.get(
+  '/send-email',
+  restoreUser,
+  (req, res) => {
+    const email = require('@sendgrid/mail');
+    email.setApiKey(send_email);
+    
+    const msg = {
+      to: '',
+      from: 'teacher@teachersaide.io',
+      subject: 'Welcome to Teachersaide.io!',
+      text: 'Thank you for signing up this app, its one of the greatest apps known to mankind.!'
+    };
+    
+    email.send(msg)
+      .then(() => console.log('Email sent'))
+      .catch((error) => console.error(error));
+      return res.json('success')
+  }
+);
 
 module.exports = router;
