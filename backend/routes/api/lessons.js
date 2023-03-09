068814b75bb333aa0596cfbfbd368e5e1cff0f32
Drew Griffin <drew.griffin.dev@gmail.com>
2023-03-08 22:03:29 -0600

    Update DB to increase allowed size of char and fixed bug

--- backend/routes/api/lessons.js
+++ backend/routes/api/lessons.js
@@ -103,11 +103,8 @@ router.post(
 
         // Store lesson plan in database
         let worksheet = await fetchAiWorksheet(grade, subject, topic, worksheetType, selectedOptions)
-        console.log('____________________________')
-        console.log(worksheet);
-        console.log('____________________________')
         const worksheetContent = await worksheet['content']
-        const response = await worksheet;
+        const response = await worksheetContent;
         const submittedPrompt = await SubmittedPrompts.add({ prompt, response, userId, promptToken });
 
         // Set cookie with lesson plan ID
